ac9210b7a29795b499b0883297403673e734e2d7
ernestchan830 <ernestchan830@gmail.com>
2019-07-01 12:02:12 -0700

    True and False input.

--- app/src/main/java/ernestchan/checkpoint_3/Question.java
+++ app/src/main/java/ernestchan/checkpoint_3/Question.java
@@ -1,39 +1,33 @@
 package ernestchan.checkpoint_3;
 
-public class Question
+import java.util.Scanner;
 
-{
+public class Question {
     private String mText;
     private String[] mFillAnswers;
     // 0 = read in boolean, 1 = read in string, 2 = read in number
     private int mAnswerType;
 
-    public Question(String text)
-    {
+    public Question(String text) {
         mText = text;
     }
 
 
-    public Question(String text, String... ans)
-    {
+    public Question(String text, String... ans) {
         mText = text;
         mFillAnswers = ans;
         mAnswerType = 1;
     }
 
     // stub method - intentionally does nothing
-    public boolean checkAnswer(boolean boolResponse)
-    {
+    public boolean checkAnswer(boolean boolResponse) {
         return false;
     }
 
     // fill the blank question check
-    public boolean checkAnswer(String userAnswer)
-    {
-        for (String ans : getFillAnswers())
-        {
-            if (ans.equalsIgnoreCase(userAnswer))
-            {
+    public boolean checkAnswer(String userAnswer) {
+        for (String ans : getFillAnswers()) {
+            if (ans.equalsIgnoreCase(userAnswer)) {
                 return true;
             }
         }
@@ -41,25 +35,26 @@ public class Question
         return false;
     }
 
-    public int getAnswerType()
-    {
+    public int getAnswerType() {
         return mAnswerType;
     }
 
-    public String[] getFillAnswers()
-    {
+    public String[] getFillAnswers() {
         return mFillAnswers;
     }
 
-    public String getText()
-    {
+    public String getText() {
         return mText;
     }
 
-    public void setText(String text)
-    {
+    public void setText(String text) {
         mText = text;
     }
 
+    // stub method
+    public boolean readInputAndCheckAnswer(Scanner input)
+    {
+        return false;
+    }
 
 }
--- app/src/main/java/ernestchan/checkpoint_3/QuizOOPMain.java
+++ app/src/main/java/ernestchan/checkpoint_3/QuizOOPMain.java
@@ -8,9 +8,9 @@ public class QuizOOPMain
     public static void main(String[] args)
     {
         Question[] questions = new Question[3];
-        questions[0] = new ExampleUnitTest.TrueFalseQuestion("Laney is better than BCC?", true);
-        questions[1] = new ExampleUnitTest.TrueFalseQuestion("Java is an easy to understand language?", false);
-        questions[2] = new Question("Which season will the F building be open?", "fall", "Fall 2019", "Maybe never");
+        questions[0] = new TrueFalseQuestion("Laney is better than BCC?", true);
+        questions[1] = new TrueFalseQuestion("Java is an easy to understand language?", false);
+        questions[2] = new FillTheBlankQuestion("Which season will the F building be open?", "fall", "Fall 2019", "Maybe never");
 
         int index = 0;
         int score = 0;
@@ -23,45 +23,14 @@ public class QuizOOPMain
         {
             System.out.println(questions[index].getText());
 
-            // If i should read a boolean
-            if (questions[index].getAnswerType() == 0)
-            {
-                System.out.print("Enter t/f: ");
-                String in = input.nextLine();
-                boolean boolResponse;
-
-                if (in.equals("t") || in.equals("true"))
-                {
-                    boolResponse = true;
-                }
-                else
-                {
-                    boolResponse = false;
-                }
-
-                if (questions[index].checkAnswer(boolResponse))
-                {
-                    System.out.println("You are correct!");
-                }
-                else
-                {
-                    System.out.println("You are incorrect!");
-                }
-            }
-            else if (questions[index].getAnswerType() == 1)
-            {
-                System.out.print("Enter the blank: ");
-                String response = input.nextLine();
-
-                if (questions[index].checkAnswer(response))
-                {
-                    System.out.println("You are correct!");
-                }
-                else
-                {
-                    System.out.println("You are incorrect!");
-                }
-            }
+//            if (questions[index].readInputAndCheckAnswer(input))
+//            {
+//                System.out.println("You are correct!");
+//            }
+//            else
+//            {
+//                System.out.println("You are incorrect!");
+//            }
 
             index++;
         }
